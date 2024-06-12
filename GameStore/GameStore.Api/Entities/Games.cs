c8e2ca1113ee760c5be9a220b049c1c0965ac970
Sandip <srai68141@gmail.com>
2024-06-12 21:56:15 +0530

    feat: added data annotation for server side validation

--- GameStore/GameStore.Api/Entities/Games.cs
+++ GameStore/GameStore.Api/Entities/Games.cs
@@ -1,12 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GameStore.Api.Entities;
 
 public class Games
 {
     public int Id { get; set; }
+    [Required]
+    [StringLength(50)]
     public required string Name { get; set; }
+
+    [Required]
+    [StringLength(25)]
     public required string Genre { get; set; }
+
+    [Required]
+    [Range(1, 100)]
     public decimal Price { get; set; }
     public DateTime ReleaseDate { get; set; }
+
+    [Url]
+    [StringLength(100)]
     public required string ImageUri { get; set; }
 
 }
