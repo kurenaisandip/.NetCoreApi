5a1042552cc4d87570da1c56848e93ed40c860a5
Sandip <srai68141@gmail.com>
2024-07-08 22:00:29 +0545

    applying migration on startup

--- GameStore/GameStore.Api/Program.cs
+++ GameStore/GameStore.Api/Program.cs
@@ -1,5 +1,7 @@
+using GameStore.Api.Data;
 using GameStore.Api.EndPoints;
 using GameStore.Api.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +12,14 @@ builder.Services.AddSqlServer<GameStoreContext>(connString);
 
 var app = builder.Build();
 
+//instead of this a new extension method is created, to keep the code clean 
+// using (var scope = app.Services.CreateScope())
+// {
+//     var dbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
+//     dbContext.Database.Migrate();
+// }
+
+app.Services.InitializeDb();
 
 app.MapGamesEndPoints();
 
